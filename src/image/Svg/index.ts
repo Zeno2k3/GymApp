570268b5afc5fe9961a4124a6d8acab5da07329e
MinhQuan.dev <mquan592003@gmail.com>
2024-12-06 16:01:33 +0700

    100% Tabbar

--- src/image/Svg/index.ts
+++ src/image/Svg/index.ts
@@ -10,6 +10,16 @@ import Intermediate from '../Svg/Intermediate.svg'
 import Goat from '../Svg/Goat.svg'
 import Athlete2 from '../Svg/Athlete2.svg'
 import FitnestX from '../Svg/FitnestX.svg'
+import Icon from '../Svg/Icon.svg'
+import Iconlink from '../Svg/Iconlink.svg'
+import Workouts from '../Svg/Workouts.svg'
+import Workoutslink from '../Svg/Workoutslink.svg'
+import Repostlink from '../Svg/Repostlink.svg'
+import Repost from '../Svg/Repost.svg'
+import Foodlink from '../Svg/Foodlink.svg'
+import Food from '../Svg/Food.svg'
+import Settingslink from '../Svg/Settingslink.svg'
+import Settings from '../Svg/Settings.svg'
 export {
     Athlete,
     LightlyActive,
@@ -23,4 +33,14 @@ export {
     Goat,
     Athlete2,
     FitnestX,
+    Icon,
+    Iconlink,
+    Workouts,
+    Workoutslink,
+    Repostlink,
+    Repost,
+    Food,
+    Foodlink,
+    Settings,
+    Settingslink
 }
--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import MyPlanNavigator from './MyPlanNavigator';
@@ -7,13 +7,63 @@ import WorkoutsNavigator from './WorkoutsNavigator';
 import RepostNavigator from './RepotsNavigator';
 import FoodNavigator from './FoodNavigator';
 import SettingsNavigator from './SettingsNavigator';
+import { appColors } from '../constants/appColor';
+import { TextComponent } from '../components';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import { Food, Foodlink, Icon, Iconlink, Repost, Repostlink, Settings, Settingslink, Workouts, Workoutslink } from '../image/Svg';
 
 const TabNavigator = () => {
     const Tab = createBottomTabNavigator();
     return (
-        <Tab.Navigator screenOptions={{
-            headerShown: false
-        }}>
+        <Tab.Navigator screenOptions={({route}) => ({
+            headerShown: false,
+            tabBarStyle: {
+                height: 80,
+                justifyContent: 'center',
+                alignItems: 'center'
+            },
+            tabBarIcon:({focused, color, size}) => {
+                let icon: ReactNode;
+                color = focused ? appColors.blue : appColors.textBtnLog
+                switch (route.name) {
+                    case 'MyPlan':
+                    icon =  focused ? <Iconlink /> : <Icon/> 
+                    break;
+                }
+                switch (route.name) {
+                    case 'Workouts':
+                    icon =  focused ? <Workoutslink /> : <Workouts/> 
+                    break;
+                }
+                switch (route.name) {
+                    case 'Report':
+                    icon =  focused ? <Repostlink /> : <Repost/> 
+                    break;
+                }
+                switch (route.name) {
+                    case 'Food':
+                    icon =  focused ? < Foodlink/> : <Food/> 
+                    break;
+                }
+                switch (route.name) {
+                    case 'Settings':
+                    icon =  focused ? <Settingslink /> : <Settings/> 
+                    break;
+                }
+                return icon;
+            },
+            tabBarLabel({focused}) {
+                return (
+                    <TextComponent
+                        text= {route.name}
+                        color= {focused ? appColors.blue : appColors.textBtnLog}
+                        styles= {{
+                            marginBottom: 12,
+                        }}
+                    />
+                )
+            },
+        })}>
             <Tab.Screen name= 'MyPlan' component={MyPlanNavigator} />
             <Tab.Screen name= 'Workouts' component={WorkoutsNavigator} />
             <Tab.Screen name= 'Report' component={RepostNavigator} />
